9bd38b8f258b1cb57e88d8b2659b80efb10f6dad
yaochoal <32651875+yaochoal@users.noreply.github.com>
2018-03-14 10:55:06 -0500

    correcionesentidaduser
    correciones entidad user y algunos ejempls en seed

--- app/models/career.rb
+++ app/models/career.rb
@@ -1,5 +1,6 @@
 class Career < ApplicationRecord
     has_many :users
     has_many :career_has_courses
+	has_many :users
     has_many :courses, through: :career_has_courses
 end
---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+
+Career.create(name: 'Ingenieria de sistemas', studyplan: 2879 )
+
+
+usuario1 = User.create(username: 'Yesid', password: '81831000' , email: 'yaochoal', career_id: 1 )
+usuario2 = User.create(username: 'Alejandra', password: '81831000' , email: 'nidia', career_id: 1 )
+usuario3 = User.create(username: 'Heyner', password: '81831000', email: 'hmartineza', career_id: 1 )
+usuario4 = User.create(username: 'Ivan', password: '81831000' , email: 'oscar', career_id: 1)
