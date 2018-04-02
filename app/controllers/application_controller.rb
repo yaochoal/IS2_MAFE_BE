372b1a27e4c003ae65e2631644f36f3b77ccdbb6
yaochoal <32651875+yaochoal@users.noreply.github.com>
2018-04-01 21:51:41 -0500

    Heyner Mailer Registro
    Implementacion de Heyner, mailer despues del registro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
+require "application_responder"
+
 class ApplicationController < ActionController::API
-	include Knock::Authenticable
-	undef_method :current_user
+  self.responder = ApplicationResponder
+  respond_to :html
+  include ActionController::MimeResponds
+  include Knock::Authenticable
+  undef_method :current_user
 end
