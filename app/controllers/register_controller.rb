cbe2bfc350af6c29c0e838170c17097494eb0b10
Unknown <hsmartineza@unal.edu.co>
2018-04-01 20:22:31 -0500

    Correcion envio mail

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -6,8 +6,6 @@ class RegisterController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-    
-  
     if @user.save
       render json: @user, status: :created, location: @user 
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,14 +16,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
+    respond_to do |format|
+      if @user.save
       #Tell the UserMail to send a welcome email after save
-      UserMailer.welcome_email(@user).deliver_now
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+        UserMailer.welcome_mail(user).deliver_now
+        render json: @user, status: :created, location: @user 
+      else
+        render json: @user.errors, status: :unprocessable_entity 
     end
   end
+end
 
   # PATCH/PUT /users/1
   def update
