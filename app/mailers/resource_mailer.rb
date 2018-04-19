1e37e4d02bdcfa907555df38fd109f5147618770
Unknown <hsmartineza@unal.edu.co>
2018-04-19 10:33:57 -0500

    Implementaacion 2 parte mailer Resources

--- app/mailers/resource_mailer.rb
+++ app/mailers/resource_mailer.rb
@@ -3,13 +3,9 @@ class ResourceMailer < ApplicationMailer
 
     def new_resource(resource)
         @resource = resource
-        themes = @resource.themes
-        @courses = themes.course
-        @careers = @courses.careers
-        @users = @careers.users
-        @recipients = @users
+        @user = @resource.user
         @url = 'http://localhost:3001'
-        emails = @recipients.collect(&:email).join(",")
-        mail(to: emails, subject: 'Aviso nuevo recurso agregado')
+ 
+        mail(to: @user.email , subject: 'Aviso nuevo recurso agregado')
     end
 end
--- test/mailers/previews/resource_mailer_preview.rb
+++ test/mailers/previews/resource_mailer_preview.rb
@@ -5,7 +5,7 @@ class ResourceMailerPreview < ActionMailer::Preview
     def new_resource
         user = User.create(username:'wer', email:'hsmartineza@hotmail.com', password:'1234', password_confirmation: '1234', career_id: 2)
 
-        ResourceMailer.new_resource(Resource.first)
+        ResourceMailer.new_resource(Resource.first.user)
     end
 
 end
